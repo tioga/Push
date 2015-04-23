3b5ad50896d8ca0baf472e3b7d179054d2800aba
Jacob D. Parr <me@jacobparr.com>
2015-04-23 16:19:53 -0700

    Refactor: removed unused imports.

--- tioga-push-engine/src/main/java/org/tiogasolutions/push/engine/core/system/CpReaderWriterProvider.java
+++ tioga-push-engine/src/main/java/org/tiogasolutions/push/engine/core/system/CpReaderWriterProvider.java
@@ -5,22 +5,20 @@
  */
 package org.tiogasolutions.push.engine.core.system;
 
-import org.tiogasolutions.push.engine.core.resources.api.ApiResourceV1;
+import org.tiogasolutions.lib.jaxrs.jackson.JacksonReaderWriterProvider;
 import org.tiogasolutions.push.common.system.AppContext;
+import org.tiogasolutions.push.engine.core.resources.api.ApiResourceV1;
 import org.tiogasolutions.push.pub.common.Push;
 import org.tiogasolutions.push.pub.common.PushResponse;
 import org.tiogasolutions.push.pub.common.UserAgent;
-import org.tiogasolutions.lib.jaxrs.jackson.JacksonReaderWriterProvider;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
-import java.util.Collections;
 
-import static java.util.Collections.*;
+import static java.util.Collections.singletonList;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
