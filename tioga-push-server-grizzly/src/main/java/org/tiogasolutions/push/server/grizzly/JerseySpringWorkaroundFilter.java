3a4567ba61a1992e1d4b076e5a83a42fa5d88e2c
Jacob D Parr <me@jacobparr.com>
2016-03-13 00:10:52 -0800

    Picking up dependencies

--- tioga-push-server-grizzly/src/main/java/org/tiogasolutions/push/server/grizzly/JerseySpringWorkaroundFilter.java
+++ tioga-push-server-grizzly/src/main/java/org/tiogasolutions/push/server/grizzly/JerseySpringWorkaroundFilter.java
@@ -11,6 +11,7 @@ import javax.ws.rs.ext.Provider;
 @Provider
 @PreMatching
 @Priority(1)
+/** See https://java.net/jira/browse/JERSEY-2726 */
 public class JerseySpringWorkaroundFilter extends RequestContextFilter {
 
   @Inject
